90481eda407cd399550e9d8af937cd857aeae3d4
Saul Howard <saul@saulhoward.com>
2024-06-16 17:02:48 +0100

    add step to test

--- tests/case-upload.spec.ts
+++ tests/case-upload.spec.ts
@@ -1,24 +1,33 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
 test.setTimeout(120000);
 
-test('has title', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+test("has title", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
   await expect(page).toHaveTitle(/Anterior/);
 });
 
-test('submit files and open case', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+test("submit files and open case", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
 
-  await page.getByRole('button', { name: /Medical Record Upload/i }).click();
-  await page.getByRole('button', { name: /Guidelines Upload/i }).click();
-  await page.getByRole('button', { name: /Open case report/i }).click();
-  
-  await expect(page.getByText('Anterior has received your case...')).toBeVisible();
-  await expect(page.getByText('Florence is processing your case...')).toBeVisible({timeout: 30000});
-  await expect(page.getByText('Mickey Mouse')).toBeVisible({timeout: 60000});
-  await expect(page.getByText('Case is not met')).toBeVisible();
+  await page.getByRole("button", { name: /Medical Record Upload/i }).click();
+  await page.getByRole("button", { name: /Guidelines Upload/i }).click();
+  await page.getByRole("button", { name: /Open case report/i }).click();
 
-  await page.getByRole('button', { name: /Choose one of the following/i }).click();
-  await expect(page.getByText('Suggest trying Medial Branch Block')).toBeVisible();
+  await expect(
+    page.getByText("Anterior has received your case..."),
+  ).toBeVisible();
+  await expect(
+    page.getByText("Florence is processing your case..."),
+  ).toBeVisible({ timeout: 30000 });
+  await expect(page.getByText("Mickey Mouse")).toBeVisible({ timeout: 60000 });
+  await expect(page.getByText("Case is not met")).toBeVisible();
+
+  await page
+    .getByRole("button", { name: /Choose one of the following/i })
+    .first()
+    .click();
+  await expect(
+    page.getByText("Suggest trying Medial Branch Block"),
+  ).toBeVisible();
 });
